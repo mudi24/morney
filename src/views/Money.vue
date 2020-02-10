d247fd9f26dbbf229be5660277df058d27e3f4b0
Irelia <yaokai729@outlook.com>
2020-02-10 11:33:51 +0800

    模拟数据迁移，版本升级

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,12 +22,28 @@ import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Prop, Watch } from "vue-property-decorator";
 
+const recordList: Record[] = JSON.parse(
+  window.localStorage.getItem("recordList") || "[]"
+);
+
+// 数据迁移
+// const version = window.localStorage.getItem("version");
+// if (version === "0.0.1") {
+//   // 数据库升级，数据迁移
+//   recordList.forEach(record => {
+//     record.createAt = new Date(2020, 0, 1);
+//   });
+//   // 保存数据
+//   window.localStorage.setItem("recordList", JSON.stringify(recordList));
+// }
+// window.localStorage.setItem("version", "0.0.2");
+
 type Record = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
-  createAt?: Date;
+  createAt?: Date; // TS 还可以写类(构造函数)
 };
 
 @Component({
@@ -35,9 +51,7 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
-  recordList: Record[] = JSON.parse(
-    window.localStorage.getItem("recordList") || "[]"
-  );
+  recordList: Record[] = recordList;
   record: Record = {
     tags: [],
     notes: "",
