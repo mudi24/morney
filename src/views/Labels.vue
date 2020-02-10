27b93b9737b3be8c09034c78489bd95a826864ff
Irelia <yaokai729@outlook.com>
2020-02-10 17:33:34 +0800

    完成 Label 组件结构和样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,5 +1,27 @@
 <template>
-  <Layout>labels</Layout>
+  <Layout>
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"></Icon>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"></Icon>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"></Icon>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"></Icon>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -9,4 +31,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg {
+      columns: #666;
+      width: 18px;
+      height: 18px;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
 </style>
